296ff9e1f5f45406867b494b8cdc9bd12b5656d1
Success <successsimeon484@gmail.com>
2024-03-24 12:51:11 +0100

    fdi

--- fdi_app/views.py
+++ fdi_app/views.py
@@ -71,18 +71,19 @@ def register(request):
 @login_required
 def profile(request):
     if  request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
+        # u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if p_form.is_valid() and u_form.is_valid() :
-            u_form.save()
+        if p_form.is_valid():
+            #  and u_form.is_valid()
+            # u_form.save()
             p_form.save()
             messages.success(request, f'Your account is updated')
             return redirect('profile')
     else:
-        u_form = UserUpdateForm(instance=request.user)
+        # u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
-        'u_form' : u_form,
+        # 'u_form' : u_form,
         'p_form' : p_form
     }
     return render(request, 'main/profile.html', context)
