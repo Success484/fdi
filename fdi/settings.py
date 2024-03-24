a2d17f12fc81994be1b875d2a7b934b0491b4bcb
Success <successsimeon484@gmail.com>
2024-03-24 01:11:52 +0100

    fdi banking

--- fdi/settings.py
+++ fdi/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fdi_app.apps.FdiAppConfig',
     'crispy_forms', 
-    'crispy_bootstrap5', # add this
+    'crispy_bootstrap5', 
 ]
 
 MIDDLEWARE = [
