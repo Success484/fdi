77fdf0bc832f3492d9ed28892a3e7c7b5cbb34bc
Success <successsimeon484@gmail.com>
2024-11-17 10:55:36 +0100

    feat: updating

--- fdi_app/forms.py
+++ fdi_app/forms.py
@@ -4,11 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Tranfer
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, required=True, label="First Name")
+    last_name = forms.CharField(max_length=30, required=True, label="Last Name")
+    email = forms.EmailField(required=True, label="Email Address")
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+        if commit:
+            user.save()
+        return user
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
--- fdi_app/views.py
+++ fdi_app/views.py
@@ -34,12 +34,11 @@ def history(request):
 
 
 def register(request):
-    if  request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to login')
+            messages.success(request, "Your account has been created! You can now log in.")
             return redirect('login')
     else:
         form = UserRegisterForm()
